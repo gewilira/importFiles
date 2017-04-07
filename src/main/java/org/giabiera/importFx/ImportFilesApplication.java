d70107b571721e1017c0315087701399b969026c
Gewil Ira <gewilira.abiera@gmail.com>
2017-04-08 03:32:12 +0400

    initial project setup

---
+++ src/main/java/org/giabiera/importFx/ImportFilesApplication.java
@@ -0,0 +1,16 @@
+package org.giabiera.importFx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ImportFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportFilesApplication.class, args);
+
+	}
+
+}
